3bf7c2a0632aba097741ecd4fa1cce2e768414ef
Ren Wei <21472761@qq.com>
2017-02-10 16:58:49 +0800

    update source code

--- vector_2.3.1_2/src/main.c
+++ vector_2.3.1_2/src/main.c
@@ -1,14 +1,21 @@
+/*
+There is a non-decreasing vector. Please write a function of deleting same element to keep one.
+
+已知一个向量中的元素按元素值非递减有序排序，编写一个函数删除向量中多余的值相同的元素。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+#define M 16
+
 static int delete(int* vector, int n) {
 	int i = 0;
-	while (i < n - 1){
+	while (i < n){
 		if (vector[i] != vector[i + 1]) {
 			i++;
 		}
 		else {
-			for (int j = i; j < n - 1; j++) {
+			for (int j = i; j < n; j++) {
 				vector[j] = vector[j + 1];
 			}
 
@@ -26,12 +33,13 @@ void output(int* vector, int n){
 }
 
 int main(int argc, char** argv) {
-	int vector[16] = { 0 };
+	int vector[M] = { 0 };
 	int n;
-	printf("please input data size:");
+	printf("please input vector size:");
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
+		printf("input a data:");
 		scanf("%d", &vector[i]);
 	}
 

d6ca82d6dfe5883b2035d79ffb51181f628f6f5a
Ren Wei <21472761@qq.com>
2017-02-13 12:10:09 +0800

    update code

--- vector_2.3.1_8/src/main.c
+++ vector_2.3.1_8/src/main.c
@@ -1,5 +1,14 @@
+/*
+There are two vectors that A has m elements while B has n elements, All the elements are in an ascending order. 
+Please write a function to merge them to vector C in which elements are also in an ascending order.
+
+有两个向量A（有m个元素）和B（有n个元素），其元素均以从小到大升序排序，编写一个函数将他们合并成一个向量C，要求C的元素也是从小到大升序排序。
+*/
 #include <stdio.h>
 #include <stdlib.h>
+
+#define M1 16
+#define M2 32
 
 static int link(int* vector_a, int m, int* vector_b, int n, int* vector_c){
 	int i = 0, j = 0, k = 0;
@@ -42,21 +51,22 @@ static int link(int* vector_a, int m, int* vector_b, int n, int* vector_c){
 }
 
 int main(int argc, char** arhv){
-	int vector_a[16] = { 0 };
-	int vector_b[16] = { 0 };
-	int vector_c[32] = { 0 };
-
+	int vector_a[M1] = { 0 };
+	int vector_b[M1] = { 0 };
+	int vector_c[M2] = { 0 };
 	int m = 0, n = 0;
 
-	printf("please input vector_a data size:");
+	printf("please input vector_a size:");
 	scanf("%d", &m);
 	for (int i = 0; i < m; i++){
+		printf("input a int element:");
 		scanf("%d", &vector_a[i]);
 	}
 
-	printf("please input vector_b data size:");
+	printf("please input vector_b size:");
 	scanf("%d", &n);
 	for (int j = 0; j < n; j++){
+		printf("input a int element:");
 		scanf("%d", &vector_b[j]);
 	}
 

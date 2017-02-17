2b0109618ffac18cffb94ebbd447f978bdc6797d
Ren Wei <21472761@qq.com>
2017-02-17 09:56:10 +0800

    update code

--- link_3.3.1_1/src/main.c
+++ link_3.3.1_1/src/main.c
@@ -1,3 +1,9 @@
+/*
+There is a signal link whose data value may be equal. A head pointer can be used, and please write a function to count the number of nodes 
+whose data value is equal to x.
+
+有一个单链表（不同结点的数据域值可能相同），其头指针为head，编写一个函数计算数据域为x的结点个数。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,13 +13,14 @@ typedef struct _node {
 	struct _node* next;
 } node;
 
+//建立单链表, 0表示结束
 node* create() {
 	int cycle = 1;
 	node *head = NULL, *s, *p = NULL;
 	int x = 0;
 
 	while (cycle) {
-		printf("input a value:");
+		printf("input a int data:");
 		scanf("%d", &x);
 		if (x == 0) {
 			cycle = 0;
